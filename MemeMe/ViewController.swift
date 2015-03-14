5d7d502adf51869def9e8aa5e4fe0396057fd488
Andrew Bell <fiixed@gmail.com>
2015-03-13 20:38:27 -0500

    add UIImagePickerController

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var memImageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    var pickerController:UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.pickerController = UIImagePickerController()
+        self.pickerController.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        if(!UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)) {
+            cameraButton.enabled = false
+        }
     }
-
-
+    
+    @IBAction func pickCamera(sender: UIBarButtonItem) {
+       
+            self.pickerController.sourceType = UIImagePickerControllerSourceType.Camera
+            self .presentViewController(self.pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAlbum(sender: UIBarButtonItem) {
+        self.pickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(self.pickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.memImageView.image = image
+        }
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
